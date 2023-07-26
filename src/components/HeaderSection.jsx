d4d576507e4119ef5935f966ba86c3d9bd6682e3
Jerome Laflamme <jeromelaflammep@gmail.com>
2023-07-26 09:32:53 -0400

    qc

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,56 +1,101 @@
 import { motion } from "framer-motion";
 import "../assets/main.css";
+import { useEffect, useState } from "react";
+import StopCircleIcon from '@mui/icons-material/StopCircle';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+
 
 export default function HeaderSection() {
+  const [animatedText, setAnimatedText] = useState(true);
+  const toggleAnimation = () => {
+    setAnimatedText(!animatedText);
+  };
+
+  const [showAnmiationToggle, setShowAnimationToggle] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolledToBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight - window.innerHeight;
+      setShowAnimationToggle(isScrolledToBottom);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <header>
       <div className="header-title-container">
-        <motion.div
-          className="header-title"
-          animate={{
-            x: [600, -1050], // initial state, final state
-            rotate: 0,
-            scale: 1,
-          }}
-          transition={{
-            duration: 7,
-            ease: "linear",
-            repeat: Infinity,
-          }}
-        >
-          <h1>JEROME</h1>
-          <h1>LAFLAMME</h1>
-          <div>
-            <svg width="15" height="15" viewBox="0 0 100 100">
-              <circle cx="50" cy="50" r="50" fill="var(--grey)" />
-            </svg>
-          </div>
-          <h1>portfolio</h1>
-        </motion.div>
-        <motion.div
-          className="header-title-duplicate"
-          animate={{
-            x: [600, -1050], // initial state, final state
-            rotate: 0,
-            scale: 1,
-          }}
-          transition={{
-            duration: 7,
-            delay: 3.5,
-            ease: "linear",
-            repeat: Infinity,
-          }}
-        >
-          <h1>JEROME</h1>
-          <h1>LAFLAMME</h1>
-          <div>
-            <svg width="15" height="15" viewBox="0 0 100 100">
-              <circle cx="50" cy="50" r="50" fill="var(--grey)" />
-            </svg>
-          </div>
-          <h1>portfolio</h1>
-        </motion.div>
+        {animatedText ? (
+          <>
+            <motion.div
+              className="header-title"
+              animate={{
+                x: [600, -1050], // initial state, final state
+                rotate: 0,
+                scale: 1,
+              }}
+              transition={{
+                duration: 9,
+                ease: "linear",
+                repeat: Infinity,
+              }}
+            >
+              <h1>JEROME</h1>
+              <h1>LAFLAMME</h1>
+              <div>
+                <svg width="15" height="15" viewBox="0 0 100 100">
+                  <circle cx="50" cy="50" r="50" fill="var(--grey)" />
+                </svg>
+              </div>
+              <h1>portfolio</h1>
+            </motion.div>
+            <motion.div
+              className="header-title-duplicate"
+              animate={{
+                x: [600, -1050], // initial state, final state
+                rotate: 0,
+                scale: 1,
+              }}
+              transition={{
+                duration: 9,
+                delay: 4.5,
+                ease: "linear",
+                repeat: Infinity,
+              }}
+            >
+              <h1>JEROME</h1>
+              <h1>LAFLAMME</h1>
+              <div>
+                <svg width="15" height="15" viewBox="0 0 100 100">
+                  <circle cx="50" cy="50" r="50" fill="var(--grey)" />
+                </svg>
+              </div>
+              <h1>portfolio</h1>
+            </motion.div>
+          </>
+        ) : (
+          <>
+            <div className="stacked">
+              <div className="header-title-name">
+                <h1>JEROME</h1>
+                <h1>LAFLAMME</h1>
+              </div>
+              <h1>portfolio</h1>
+            </div>
+
+          </>
+        )}
+
       </div>
+      {showAnmiationToggle && (
+        <div className="toggleAnimation-btn-container">
+          <button className="toggleAnimation-btn" onClick={toggleAnimation}>Animation</button>
+          {animatedText ? <StopCircleIcon fontSize="small" /> : <PlayCircleIcon fontSize="small" />}
+        </div>)}
+
     </header>
   );
 }
--- src/components/YellowSection.jsx
+++ src/components/YellowSection.jsx
@@ -6,34 +6,30 @@ import PhoneIcon from "@mui/icons-material/Phone";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
 import CodeIcon from "@mui/icons-material/Code";
 import CollectionsIcon from "@mui/icons-material/Collections";
-import { useEffect } from 'react';
 
 
 export default function YellowSection() {
-
-  // log on scroll position
-  useEffect(() => {
-    window.addEventListener('scroll', () => {
-      console.log(window.scrollY)
-    })
-  }, [])
 
   const listData = [
     {
       icon: <PhoneIcon />,
       text: "(514) 884-9516",
+      link: "tel:5148849516",
     },
     {
       icon: <AlternateEmailIcon />,
       text: "jerome@jlaflamme.com",
+      link: "mailto:jerome@jlaflamme.com"
     },
     {
       icon: <CodeIcon />,
       text: "github.com/jerome-laflamme",
+      link: "github.com/jerome-laflamme",
     },
     {
       icon: <CollectionsIcon />,
       text: "croustine.xyz",
+      link: "croustine.xyz",
     },
   ];
 
@@ -44,42 +40,41 @@ export default function YellowSection() {
         <div>
           {listData.map((item, index) => {
             return (
-              <div key={index} className="info-item">
-                <div className="info-icon">{item.icon}</div>
-                <div className="info-text">{item.text}</div>
-                
-              </div>
+                  <a key={index} href={item.link} className='info-item'>
+                    <div className="info-icon">{item.icon}</div>
+                    <div className="info-text">{item.text}</div>
+                  </a>
             );
           })}
         </div>
       </div>
       <div className="text-container">
-      <h2>profil</h2>
-      <hr />
-          <div className='text-area'>
-            <p>
-              Développeur full-stack de formation, je me passionne pour l’art et
-              le design.
-            </p>
-            <p>
-              J’aime toucher à plusieurs domaines tels que la musique, la photo,
-              les arts visuels, la modélisation 3D, le UI/UX Design, et le
-              motion design. Je porte un grand intérêt aux nouvelles
-              technologies et j'ai une soif de les apprendre. Les défis sont de
-              mises pour me garder stimulé et intéressé.
-            </p>
-            <p className='bolder'>
-              Expérience : JS, TS, React, Vue, CSS, HMTL, Three.JS, C#, MySQL,
-              MongoDB, Node.JS, Blender, Figma, Photoshop, Illustrator, After
-              Effects, Premiere Pro
-            </p>
-            <div className="color-palette-container">
-              <span className="square dark-blue"></span>
-              <span className='square light-blue'></span>
-              <span className='square red'></span>
-              <span className='square white'></span>
-            </div>
+        <h2>profil</h2>
+        <hr />
+        <div className='text-area'>
+          <p>
+            Développeur full-stack de formation, je me passionne pour l’art et
+            le design.
+          </p>
+          <p>
+            J’aime toucher à plusieurs domaines tels que la musique, la photo,
+            les arts visuels, la modélisation 3D, le UI/UX Design, et le
+            motion design. Je porte un grand intérêt aux nouvelles
+            technologies et j'ai une soif de les apprendre. Les défis sont de
+            mises pour me garder stimulé et intéressé.
+          </p>
+          <p className='bolder'>
+            Expérience : JS, TS, React, Vue, CSS, HMTL, Three.JS, C#, MySQL,
+            MongoDB, Node.JS, Blender, Figma, Photoshop, Illustrator, After
+            Effects, Premiere Pro
+          </p>
+          <div className="color-palette-container">
+            <span className="square dark-blue"></span>
+            <span className='square light-blue'></span>
+            <span className='square red'></span>
+            <span className='square white'></span>
           </div>
+        </div>
       </div>
     </div>
   );
