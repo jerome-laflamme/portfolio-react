de177d5ff287a73341aa97955c2315d964b59966
Jerome Laflamme <jeromelaflammep@gmail.com>
2023-07-31 18:25:30 -0300

    done for now

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,61 +1,57 @@
 import { motion } from "framer-motion";
 import "../assets/main.css";
 import { useEffect, useState } from "react";
-// import StopCircleIcon from '@mui/icons-material/StopCircle';
-// import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import PlayMe from "../assets/img/play-me.png";
 
 
 export default function HeaderSection() {
-  // const [animatedText, setAnimatedText] = useState(true);
-  // const toggleAnimation = () => {
-  //   setAnimatedText(!animatedText);
-  // };
-
-  // const [showAnmiationToggle, setShowAnimationToggle] = useState(false);
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const isScrolledToBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight - window.innerHeight;
-  //     setShowAnimationToggle(isScrolledToBottom);
-  //   };
-
-  //   window.addEventListener('scroll', handleScroll);
-
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //   };
-  // }, []);
-
   const [scrollY, setScrollY] = useState(0);
+
   useEffect(() => {
-    const handleScroll = () => {
+    window.addEventListener("scroll", () => {
       setScrollY(window.scrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    });
   }, []);
 
   return (
     <header>
+      <div className="play-btn">
+          <motion.button
+            className="play-btn"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "300px",
+              height: "300px",
+              transform: "rotate(0deg)",
+              transformOrigin: "center",
+            }}
+            animate={{
+              translateX: scrollY * 0.5,
+            }}
+          >
+            <motion.img
+              src={PlayMe}
+              alt="play"
+              style={{
+                width: "100%",
+                height: "100%",
+              }}
+              animate={{
+                rotate: -360,
+              }}
+              transition={{
+                duration: 15,
+                repeat: Infinity,
+                repeatType: "loop",
+                ease: "linear",
+              }}
+            />
+          </motion.button>
+      </div>
       <div className="main-animation-text">
         {/* HIDDEN ON TOP */}
-        <motion.h1
-          animate={{
-            y: scrollY * 0.5
-          }}
-          id="top-decal">
-          Port
-        </motion.h1>
-        <motion.h1
-          animate={{
-            y: scrollY * 0.8,
-          }}
-          id="bottom-decal">
-          folio
-        </motion.h1>
         <motion.h1
           animate={{
             y: scrollY * 0.5,
@@ -66,47 +62,42 @@ export default function HeaderSection() {
         </motion.h1>
         <motion.h1
           animate={{
-              y: scrollY * 0.4,
-            }}
+            y: scrollY * 0.4,
+          }}
           id="bottom-decal">
           folio
         </motion.h1>
         {/* VIEWED IN WINDOW */}
         <motion.h1
           animate={{
-              y: scrollY * 0.5
-            }}
+            y: scrollY * 0.5
+          }}
           id="top">
           Port
         </motion.h1>
         <motion.h1
           animate={{
-              y: scrollY * 0.5,
-            }}
+            y: scrollY * 0.5,
+          }}
           id="bottom">
           folio
         </motion.h1>
         <motion.h1
           animate={{
-              y: scrollY * 0.3,
-            }}
+            y: scrollY * 0.3,
+          }}
           id="top"
         >
           Port
         </motion.h1>
         <motion.h1
           animate={{
-              y: scrollY * 0.3,
-            }}
+            y: scrollY * 0.3,
+          }}
           id="bottom">
           folio
         </motion.h1>
-        <h1>test</h1>
       </div>
-      <div className="name-tag">
-        <h2>Jerome Laflamme</h2>
-      </div>
-
     </header>
   );
 }
--- src/components/YellowSection.jsx
+++ src/components/YellowSection.jsx
@@ -1,11 +1,12 @@
 import '../assets/main.css'
 import me from "../assets/img/me.jpg";
+import { motion } from "framer-motion";
 
 // Icons
 import PhoneIcon from "@mui/icons-material/Phone";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
 import CodeIcon from "@mui/icons-material/Code";
-import CollectionsIcon from "@mui/icons-material/Collections";
+// import CollectionsIcon from "@mui/icons-material/Collections";
 
 
 export default function YellowSection() {
@@ -26,28 +27,33 @@ export default function YellowSection() {
       text: "github.com/jerome-laflamme",
       link: "github.com/jerome-laflamme",
     },
-    {
-      icon: <CollectionsIcon />,
-      text: "croustine.xyz",
-      link: "croustine.xyz",
-    },
+    // {
+    //   icon: <CollectionsIcon />,
+    //   text: "croustine.xyz",
+    //   link: "croustine.xyz",
+    // },
   ];
+
 
   return (
     <div className="main-background">
-      <div className="info">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        className="info">
         <img alt="Me" src={me} />
         <div>
           {listData.map((item, index) => {
             return (
-                  <a key={index} href={item.link} className='info-item'>
-                    <div className="info-icon">{item.icon}</div>
-                    <div className="info-text">{item.text}</div>
-                  </a>
+              <a key={index} href={item.link} className='info-item'>
+                <div className="info-icon">{item.icon}</div>
+                <div className="info-text">{item.text}</div>
+              </a>
             );
           })}
         </div>
-      </div>
+      </motion.div>
       <div className="text-container">
         <h2>profil</h2>
         <hr />
@@ -69,14 +75,38 @@ export default function YellowSection() {
           </p>
           {/* Three.JS,  Illustrator, After
             Effects, Premiere Pro*/}
-          <div className="color-palette-container">
-            <span className="square dark-blue"></span>
-            <span className='square light-blue'></span>
-            <span className='square red'></span>
-            <span className='square white'></span>
+          <div className="exp-container">
+            <div className="exp">
+              <h3>La Croix Bleu</h3>
+              <p>Stagiare | Printemps 2023</p>
+            </div>
+            <div className="exp">
+              <h3>À mon compte</h3>
+              <p> Développeur web | 2021 - Présentement</p>
+            </div>
+            <div className="exp">
+              <h3>Astropof Inc</h3>
+              <p>Stagiare - Intégrateur/Developpeur web | 2021 - Présentement</p>
+            </div>
           </div>
+          <div className="school-container">
+            <div className="school">
+              <p>Collège de Shawinigan - AEC Développement logiciel | 2023</p>
+            </div>
+            <div className="school">
+              <p>Treehouse.com - Bootcamp front-end web | 2023</p>
+            </div>
+          </div>
+
+
+          <motion.div className="color-palette-container">
+            <motion.span className="square dark-blue"></motion.span>
+            <motion.span className='square light-blue'></motion.span>
+            <motion.span className='square red'></motion.span>
+            <motion.span className='square white'></motion.span>
+          </motion.div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
