e88631209e991e7623184e9a8d8d7355e104671f
Jerome Laflamme <jeromelaflammep@gmail.com>
2023-11-27 21:34:55 -0500

    qc

---
+++ src/components/GalleryComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GalleryComponent = () => {
+  return (
+    <div>
+      {/* Your gallery component code goes here */}
+    </div>
+  );
+};
+
+export default GalleryComponent;
--- src/components/SelectionMenu.jsx
+++ src/components/SelectionMenu.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import AnimatedIcon from "./AnimatedIcon";
 import Name from "./Name";
+import GalleryComponent from "./GalleryComponent";
+import AboutInfo from "./AboutInfo";
 
 const SelectionMenu = ({
   showSelection,
@@ -12,49 +14,100 @@ const SelectionMenu = ({
 }) => {
   const selectionOptions = ["Photo", "Drawing", "About"];
 
-  useEffect(() => {
-    console.log(showSelection);
-  }, [showSelection]);
+  const [showGallery, setShowGallery] = useState(false);
+  const [galleryContent, setGalleryContent] = useState([]); // [images, videos, gifs
+  const [showAbout, setShowAbout] = useState(false);
+
+  const showMagic = (e) => {
+    switch (e.target.innerText) {
+      case "PHOTO":
+        setGalleryContent("PHOTO");
+        setShowGallery(true);
+        break;
+      case "DRAWING":
+        setGalleryContent("DRAWING");
+        setShowGallery(true);
+        break;
+      case "ABOUT":
+        setShowAbout(true);
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
-      <AnimatePresence>
-        {showSelection && (
-          <motion.div
-            className="selection-container"
-            initial={{ y: showSelection ? "0" : "-100vh" }}
-            animate={{ y: showSelection ? "-100vh" : "0" }}
-            exit={{ y: showSelection ? "0" : "-100vh" }}
-            transition={{ duration: 0.7, ease: "easeInOut" }}
-          >
-            <Name nameFontSize={nameFontSize} nameColor={"white"} />
-            <AnimatedIcon
-              IconComponent={KeyboardArrowUpIcon}
-              onClick={selectionMenuToggle}
-              color={"white"}
-            />
+    <>
+    <AnimatePresence>
+      {showSelection && (
+        <motion.div
+          className="selection-container"
+          initial={{ y: showSelection ? "0" : "-100vh" }}
+          animate={{ y: showSelection ? "-100vh" : "0" }}
+          exit={{ y: showSelection ? "0" : "-100vh" }}
+          transition={{ duration: 0.7, ease: "easeInOut" }}
+        >
+          <Name nameFontSize={nameFontSize} nameColor={"white"} />
+          <AnimatedIcon
+            IconComponent={KeyboardArrowUpIcon}
+            onClick={selectionMenuToggle}
+            color={"white"}
+          />
 
-            {selectionOptions.map((option, index) => (
-              <motion.p
-                key={index}
-                style={{
-                  fontSize: selectionFontSize,
-                  fontWeight: 100,
-                  cursor: "pointer",
-                  padding: "0.5rem",
-                  marginLeft: "10%",
-                  width: "min-content",
-                  letterSpacing: "0.3em",
-                }}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.4, delay: index * 0.05 }}
-              >
-                {option}
-              </motion.p>
-            ))}
-          </motion.div>
+          {selectionOptions.map((option, index) => (
+            <motion.p
+              key={index}
+              style={{
+                fontSize: selectionFontSize,
+                fontWeight: 100,
+                cursor: "pointer",
+                padding: "0.5rem",
+                marginLeft: "10%",
+                width: "min-content",
+                letterSpacing: "0.3em",
+              }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.4, delay: index * 0.05 }}
+              onClick={showMagic}
+            >
+              {option}
+            </motion.p>
+          ))}
+        </motion.div>
+      )}
+      
+    </AnimatePresence>
+    <AnimatePresence>
+        {showGallery && (
+            <motion.div
+            className="gallery-container"
+            initial={{ y: showGallery ? "100vh" : "0" }}
+            animate={{ y: showGallery ? "0" : "100vh" }}
+            exit={{ y: showGallery ? "100vh" : "0" }}
+            transition={{ duration: 0.7, ease: "easeInOut" }}
+            >
+            <GalleryComponent
+                galleryContent={galleryContent}
+                setShowGallery={setShowGallery}
+            />
+            </motion.div>
         )}
-      </AnimatePresence>
+        </AnimatePresence>
+        <AnimatePresence>
+        {showAbout && (
+            <motion.div
+            className="about-container"
+            initial={{ y: showAbout ? "100vh" : "0" }}
+            animate={{ y: showAbout ? "0" : "100vh" }}
+            exit={{ y: showAbout ? "100vh" : "0" }}
+            transition={{ duration: 0.7, ease: "easeInOut" }}
+            >
+            <AboutInfo />
+            </motion.div>
+        )}
+        </AnimatePresence>
+    </>
   );
 };
 
